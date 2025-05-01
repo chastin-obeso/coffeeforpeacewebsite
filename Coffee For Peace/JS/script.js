9db8b514733417341db7daf9c586fbd2eb227105
chastin-obeso <151446398+chastin-obeso@users.noreply.github.com>
2025-05-01 11:28:21 +0800

    Update script.js
    Limited hover effects in footer to desktop only

--- Coffee For Peace/JS/script.js
+++ Coffee For Peace/JS/script.js
@@ -11,21 +11,29 @@ function closeNav() {
     document.getElementById("navbarlinks-mobile").style.width = "0vw";
   }
 
+const desktop = window.matchMedia("(min-width: 1025px)");
 const image = document.getElementById('fb');
   image.addEventListener('mouseover', function() {
-    image.src = '/Img/facebook-hover.png';
+    if(desktop.matches){
+      image.src = '/Img/facebook-hover.png';
+    }
   });
 
   image.addEventListener('mouseout', function() {
-    image.src = '/Img/facebook.png'; 
+    if(desktop.matches){
+      image.src = '/Img/facebook.png'; 
+    }
   });
 
 const image2 = document.getElementById('ig');
   image2.addEventListener('mouseover', function() {
-    image2.src = '/Img/instagram-hover.png';
+    if(desktop.matches){
+      image2.src = '/Img/instagram-hover.png';
+    }
   });
 
   image2.addEventListener('mouseout', function() {
-    image2.src = '/Img/instagram.png'; 
+    if(desktop.matches){
+      image2.src = '/Img/instagram.png'; 
+    }
   });
-
